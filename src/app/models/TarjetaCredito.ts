b4b26a8fb1817c3b03761f1346a56da1704cd496
rolando yanique <rolyan66@gmail.com>
2025-09-24 14:15:59 -0400

    Se realizo la instalacion de bootstrap 5 ademas de la estructura

---
+++ src/app/models/TarjetaCredito.ts
@@ -0,0 +1,17 @@
+export class TarjetaCredito{
+    id?:string;
+    titular:string;
+    numeroTarjeta:string;
+    fechaExpiracion:string;
+    cvv:number;
+    fechaCreacion:Date;
+    fechaActualizacion:Date;
+    constructor(titular:string,numeroTarjeta:string,fechaExpriracion:string,cvv:number){
+        this.titular=titular;
+        this.numeroTarjeta=numeroTarjeta;
+        this.fechaExpiracion=fechaExpriracion;
+        this.cvv=cvv;
+        this.fechaCreacion=new Date();
+        this.fechaActualizacion=new Date();
+    }
+}
