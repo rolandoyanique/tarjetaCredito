2e0d5dc42dfda2bef60ac84bdc28280cb81a47e8
rolando yanique <rolyan66@gmail.com>
2025-09-25 11:25:38 -0400

    Se realizo el modelado y la incoorporacion del formulario reactivo para la card crear tarjeta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { CrearTarjetaComponent } from './components/crear-tarjeta/crear-tarjeta.component';
 import { ListarTarjetaComponent } from './components/listar-tarjeta/listar-tarjeta.component';
@@ -12,7 +12,8 @@ import { ListarTarjetaComponent } from './components/listar-tarjeta/listar-tarje
     ListarTarjetaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
