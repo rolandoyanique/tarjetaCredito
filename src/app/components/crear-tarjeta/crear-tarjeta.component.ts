4d4417b491a006c1508c3e011289034ed943e2b9
Rolando <rolyan66@gmail.com>
2025-09-27 16:48:18 -0400

    Se realizo la adicion de las validaciones y la estructura de listado de tarjetas

--- src/app/components/crear-tarjeta/crear-tarjeta.component.ts
+++ src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TarjetaCredito } from 'src/app/models/TarjetaCredito';
 
 @Component({
   selector: 'app-crear-tarjeta',
@@ -11,12 +12,21 @@ export class CrearTarjetaComponent {
   constructor(private fb:FormBuilder){
     this.form=this.fb.group({ 
       titular:['',Validators.required], 
-      numeroTarjeta:['', Validators.required], 
-      fechaExpiracion:['', Validators.required], 
-      cvv:['', Validators.required] 
+      numeroTarjeta:['', [Validators.required,Validators.minLength(16),Validators.maxLength(16)]], 
+      fechaExpiracion:['', [Validators.required,Validators.minLength(5),Validators.maxLength(5)]], 
+      cvv:['',[Validators.required,Validators.minLength(3),Validators.maxLength(3)]] 
     })
   }
 crearTarjeta(){
-  console.log(this.form);
+  const TARJETA:TarjetaCredito={
+    titular:this.form.value.titular,
+    numeroTarjeta:this.form.value.numeroTarjeta,
+    fechaExpiracion:this.form.value.fechaExpiracion,
+    cvv:this.form.value.cvv,
+    fechaCreacion:new Date(),
+    fechaActualizacion:new Date()
+  }
+  console.log(TARJETA);
 }
 }
+
